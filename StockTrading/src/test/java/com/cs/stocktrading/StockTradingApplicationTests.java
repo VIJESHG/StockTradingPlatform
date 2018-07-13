776ba5823f31950b0af04ecb91d7c6fe7fea680f
Student <ghandarevb14.comp@coep.ac.in>
2018-07-13 00:50:25 -0400

    final testing done

--- StockTrading/src/test/java/com/cs/stocktrading/StockTradingApplicationTests.java
+++ StockTrading/src/test/java/com/cs/stocktrading/StockTradingApplicationTests.java
@@ -5,12 +5,76 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class StockTradingApplicationTests {
+import org.springframework.security.web.server.authentication.ServerAuthenticationFailureHandler;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
-	@Test
-	public void contextLoads() {
-	}
+import org.springframework.test.context.junit4.SpringRunner;
+ 
+import io.restassured.RestAssured;
+ 
+import static io.restassured.RestAssured.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+ 
+//import static com.jayway.restassured.RestAssured.get;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class StockTradingApplicationTests {
+@LocalServerPort 
+private int serverPort;
+@Before
+public void init() {
+RestAssured.port = serverPort;
+}
+@Test
+public void checkTraders() {
+given().auth().basic("admin", "admin")
+        .when().get("/trader")
+        .then().statusCode(200);
+
+given().auth().basic("admin", "admin")
+.when().get("/trader/1")
+.then().statusCode(200);
+
+}
+
+@Test
+public void checkCompany() {
+	given().auth().basic("admin", "admin")
+    .when().get("/company")
+    .then().statusCode(200);
+
+	
+	given().auth().basic("admin", "admin")
+    .when().get("/company/AP")
+    .then().statusCode(200);
+
+}
+
+@Test 
+public void checkOrder() {
+	
+	given().auth().basic("admin", "admin")
+    .when().get("/admin/order/1")
+    .then().statusCode(200);
+	
+//	given().auth().basic("trader", "trader")
+  //  .when().get("/order/cancel/1")
+    //.then().statusCode(200);
+
+	
+	given().auth().basic("trader", "trader")
+    .when().get("/order/list/sort/price/asc")
+    .then().statusCode(200);
+
+	
+}
 
 }
